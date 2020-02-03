e9d0ee8bb57bf2470c26caa978487fd7ce2a6978
최준우 <junwoochoi@zuminternet.com>
2020-02-03 14:28:26 +0900

    feat: setup auth server

--- gateway/src/test/java/com/alpha/gateway/utils/JwtUtilsTest.java
+++ gateway/src/test/java/com/alpha/gateway/utils/JwtUtilsTest.java
@@ -1,8 +1,19 @@
 package com.alpha.gateway.utils;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JwtUtilsTest {
 
@@ -17,5 +28,21 @@ class JwtUtilsTest {
     void testNotValidToken() {
         final boolean valid = JwtUtils.isValid("bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjm0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.k4DFETucHbH3ClcTt4NoiODIX_bjzcQtA_vt7xNbygg");
         assertThat(valid).isFalse();
+    }
+
+    @Test
+    @DisplayName("만료된 토큰은 parse에 실패한다")
+    void expiredToken() {
+        final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        final String jwt = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setExpiration(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+         assertThrows(ExpiredJwtException.class, () ->
+                Jwts.parser().setSigningKey(key).parse(jwt)
+        );
+
     }
 }
